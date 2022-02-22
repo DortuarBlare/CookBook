d4e73e24c0f10a77ebc7bb376a853a2bba97c3ef
DortuarBlare <YaroslavtsevNik2000@gmail.com>
2022-02-22 16:26:27 +0700

    updated gishType entity

--- src/main/java/com/groupproject/nstu/cookbook/entity/DishType.java
+++ src/main/java/com/groupproject/nstu/cookbook/entity/DishType.java
@@ -1,4 +1,48 @@
 package com.groupproject.nstu.cookbook.entity;
 
-public class DishType {
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "Dish_type")
+public class DishType implements Serializable {
+
+    @Id
+    @GenericGenerator(name = "generator", strategy = "increment")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public DishType() {
+    }
+
+    public DishType(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public DishType(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
